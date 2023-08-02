f2536a829307afc781f6a06d1a3a6034131eef48
Angeloamenta <angeloamenta7gmail.com>
2023-08-03 00:11:21 +0200

    route header ok

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationError } from '@angular/router';
 import { Location } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 
@@ -10,32 +10,58 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class HeaderComponent {
 
-  constructor(private router:ActivatedRoute, private location:Location) {}
+  currentRoute = "";
+  constructor(private router:Router, private location:Location) {
+    
+    this.router.events.subscribe((event: Event) => {
+        if (event instanceof NavigationStart) {
+            // Show progress spinner or progress bar
+            console.log('Route change detected');
+            this.currentRoute = event.url
+            console.log(this.currentRoute);
+
+            if (this.currentRoute == "") {
+              this.project = false
+              this.about = false
+            } 
+          
+            if (this.currentRoute == "/project") {
+              this.project = true
+            } else {
+              this.project = false
+            }
+          
+            if (this.currentRoute == "/about") {
+              this.about = true
+            } else {
+              this.about = false
+            }
+            
+        }
+
+        if (event instanceof NavigationEnd) {
+            // Hide progress spinner or progress bar
+            this.currentRoute = event.url;          
+            console.log(event);
+        }
+
+        if (event instanceof NavigationError) {
+             // Hide progress spinner or progress bar
+
+            // Present error to user
+            console.log(event.error);
+        }
+    });
+  }
 
   project= false
   about = false
+  routeSubscription:any
 
 ngOnInit() {
-  console.log(this.router.snapshot);
+  
   console.log(this.location.path())
   let path = this.location.path()
-
-  if (path == "") {
-    this.project = false
-    this.about = false
-  } 
-
-  if (path == "/project") {
-    this.project = true
-  } else {
-    this.project = false
-  }
-
-  if (path == "/about") {
-    this.about = true
-  } else {
-    this.about = false
-  }
 
   
 }
